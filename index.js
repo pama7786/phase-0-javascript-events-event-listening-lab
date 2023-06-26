f27b63dabb5f70c8ee5c05e7c3171609f20c3c20
Abdirahman abass diis <abdirahmanfarmajo7786@gmail.com>
2023-06-26 22:26:06 +0300

    first commit

--- index.js
+++ index.js
@@ -1,2 +1,12 @@
+// function addingEventListener() {
+    // expect(input.addEventListener.called).to.be.true;
+// }
+
+// Assuming you have a variable 'input' referencing the desired element
+
 function addingEventListener() {
-}
+    input.addEventListener('click', function() {
+      // Event handling code goes here
+    });
+  }
+  
